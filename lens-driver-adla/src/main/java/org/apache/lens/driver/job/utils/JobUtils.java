69bb377b774edb28691a073a15c352d6163b0c2c
Vishnu Venketramana Chilakamarri <vishnu.chilakamarri@im2550-x2.corp.inmobi.com>
2018-01-15 22:42:04 +0530

    adding get status and submit job

--- lens-driver-adla/src/main/java/org/apache/lens/driver/job/utils/JobUtils.java
+++ lens-driver-adla/src/main/java/org/apache/lens/driver/job/utils/JobUtils.java
@@ -1,25 +1,75 @@
 package org.apache.lens.driver.job.utils;
 
-import lombok.Data;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
 import org.apache.lens.driver.job.states.JobState;
+import org.apache.lens.server.api.error.LensException;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.io.OutputStream;
+import java.io.InputStream;
 
-@Data
 public class JobUtils {
 
-    private String baseUrl = "https://yoda.azuredatalakeanalytics.net/";
+    private static final String baseUrl = "https://yoda.azuredatalakeanalytics.net/";
 
-    public static void submitJob(String jobId, String payload, String bearerToken){
+    private static Client client = Client.create();
 
+    private static final String querySkeleton = "{  \n" +
+            "  \"jobId\": \"<<jobid>>\",  \n" +
+            "  \"name\": \"<<jobid>>\",  \n" +
+            "  \"type\": \"USql\",  \n" +
+            "  \"degreeOfParallelism\": 1,  \n" +
+            "  \"priority\": 1000,  \n" +
+            "  \"properties\": {  \n" +
+            "    \"type\": \"USql\",  \n" +
+            "    \"script\": \"<<script>>\"  \n" +
+            "  }  \n" +
+            "}  ";
+
+    public static void submitJob(String jobId, String payload, String bearerToken) throws LensException{
+        System.out.println(bearerToken.length());
+        String finalquery = querySkeleton.replace("<<script>>", payload);
+        finalquery = finalquery.replace("<<jobid>>", jobId);
+        String requestUrl = baseUrl + "jobs/" + jobId +"?api-version=2016-11-01";
+        WebResource webResource = client.resource(requestUrl);
+        WebResource.Builder x = webResource.header("Content-Type", "application/json");
+        x = x.header("Authorization",bearerToken);
+        x = x.accept("application/json");
+        ClientResponse response = x.put(ClientResponse.class,finalquery);
+        String output = response.getEntity(String.class);
+        System.out.println(output);
+        System.out.println(finalquery);
+        if (response.getStatus() != 200) throw new LensException();
     }
 
-    public static JobState getStatus(String jobId, String bearerToken) {
-        return JobState.COMPLETED;
+    public static JobState getStatus(String jobId, String bearerToken) throws LensException {
+        System.out.println(bearerToken.length());
+        String requestUrl = baseUrl + "jobs/" + jobId +"?api-version=2016-11-01";
+        WebResource webResource = client.resource(requestUrl);
+        WebResource.Builder x = webResource.header("Content-Type", "application/json");
+        x = x.header("Authorization",bearerToken);
+        x = x.accept("application/json");
+        ClientResponse response = x.get(ClientResponse.class);
+        String output = response.getEntity(String.class);
+        try {
+            JSONObject jsonObject = new JSONObject(output);
+            if (jsonObject.get("result") == null)
+                return JobState.DOES_NOT_EXIST;
+            if (jsonObject.get("result").toString().trim().equals("Succeeded"))
+                return JobState.COMPLETED;
+            if (jsonObject.get("result").toString().trim().equals("Running"))
+                return JobState.RUNNING;
+            return JobState.FAILED;
+        } catch (JSONException e) {
+            throw new LensException("Unknown error, unable to parse the result");
+        }
     }
 
-    public static OutputStream getResult(String jobId, String bearerToken) {
+    public static InputStream getResult(String jobId, String bearerToken) {
         return null;
     }
+
 
 }
