222ceea121cbc6724d30dd3ddb5a9419d4402ed8
sushrut.ikhar <sushrut.ikhar@inmobi.com>
2018-01-16 10:25:14 +0530

    jersey -> glassfish client

--- lens-driver-adla/src/main/java/org/apache/lens/driver/job/utils/JobUtils.java
+++ lens-driver-adla/src/main/java/org/apache/lens/driver/job/utils/JobUtils.java
@@ -22,15 +22,17 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import org.apache.lens.driver.job.states.JobState;
 import org.apache.lens.server.api.error.LensException;
 
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.filter.LoggingFilter;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
 
 
 public class JobUtils {
@@ -39,7 +41,7 @@ public class JobUtils {
 
   private static String baseUrl = "https://yoda.azuredatalakeanalytics.net/";
 
-  private static Client client = Client.create();
+  private static Client client = ClientBuilder.newClient(new ClientConfig().register(LoggingFilter.class));
 
   private static String querySkeleton = "{  \n"
     + "  \"jobId\": \"<<jobid>>\",  \n"
@@ -59,28 +61,30 @@ public class JobUtils {
     String finalquery = querySkeleton.replace("<<script>>", payload);
     finalquery = finalquery.replace("<<jobid>>", jobId);
     String requestUrl = baseUrl + "jobs/" + jobId + "?api-version=2016-11-01";
-    WebResource webResource = client.resource(requestUrl);
-    WebResource.Builder x = webResource.header("Content-Type", "application/json");
+    WebTarget webResource = client.target(requestUrl);
+    Invocation.Builder x = webResource.request(MediaType.APPLICATION_JSON);
     x = x.header("Authorization", bearerToken);
     x = x.accept("application/json");
-    ClientResponse response = x.put(ClientResponse.class, finalquery);
-    String output = response.getEntity(String.class);
-    System.out.println(output);
-    System.out.println(finalquery);
+    Response response = x.put(Entity.entity(finalquery, MediaType.APPLICATION_JSON));
     if (response.getStatus() != 200) {
       throw new LensException();
     }
+    String output = response.readEntity(String.class);
+    System.out.println(output);
+    System.out.println(finalquery);
   }
 
   public static JobState getStatus(String jobId, String bearerToken) throws LensException {
-    System.out.println(bearerToken.length());
     String requestUrl = baseUrl + "jobs/" + jobId + "?api-version=2016-11-01";
-    WebResource webResource = client.resource(requestUrl);
-    WebResource.Builder x = webResource.header("Content-Type", "application/json");
+    WebTarget webResource = client.target(requestUrl);
+    Invocation.Builder x = webResource.request(MediaType.APPLICATION_JSON);
     x = x.header("Authorization", bearerToken);
     x = x.accept("application/json");
-    ClientResponse response = x.get(ClientResponse.class);
-    String output = response.getEntity(String.class);
+    Response response = x.get();
+    if (response.getStatus() != 200) {
+      throw new LensException();
+    }
+    String output = response.readEntity(String.class);
     try {
       JSONObject jsonObject = new JSONObject(output);
       if (jsonObject.get("result") == null) {
@@ -106,5 +110,8 @@ public class JobUtils {
     }
   }
 
+/*  public static void main(String[] args) throws LensException {
+    JobUtils.getStatus("aa96c2e1-8f55-4fa6-b95f-ae85b25a1a0f", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ino0NHdNZEh1OHdLc3VtcmJmYUs5OHF4czVZSSIsImtpZCI6Ino0NHdNZEh1OHdLc3VtcmJmYUs5OHF4czVZSSJ9.eyJhdWQiOiJodHRwczovL21hbmFnZW1lbnQuY29yZS53aW5kb3dzLm5ldC8iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC84NzhmNmIzMS1mZWVhLTQyYmMtYTk1ZC1kNDQ5NmY5YmVmNjkvIiwiaWF0IjoxNTE2MDc3MTc2LCJuYmYiOjE1MTYwNzcxNzYsImV4cCI6MTUxNjA4MTA3NiwiYWNyIjoiMSIsImFpbyI6IlkyTmdZSWkxdWhLZ2xaYVI5cnByY2JIK2JMMzBhVDRCRlg4bWxMd1EzNlYza09YNDBob0EiLCJhbHRzZWNpZCI6IjE6bGl2ZS5jb206MDAwMzQwMDFBQzNDNzZGRiIsImFtciI6WyJwd2QiXSwiYXBwaWQiOiJjNDRiNDA4My0zYmIwLTQ5YzEtYjQ3ZC05NzRlNTNjYmRmM2MiLCJhcHBpZGFjciI6IjIiLCJlX2V4cCI6MjYyODAwLCJlbWFpbCI6InB1bmVldGd1cHRhQG91dGxvb2suY29tIiwiZmFtaWx5X25hbWUiOiJHdXB0YSIsImdpdmVuX25hbWUiOiJQdW5lZXQiLCJncm91cHMiOlsiNDQwZDg5MjctYTY0Yi00MjJiLWI2YzktNDE2NTIwYzA0ZGY1IiwiOGY1NGMwZGItMjYwZi00NWVjLTk1ZTUtMTc2MTkxYTIxNWNkIl0sImlkcCI6ImxpdmUuY29tIiwiaXBhZGRyIjoiMTQuMTQyLjEwNC4xNzAiLCJuYW1lIjoiUHVuZWV0IEd1cHRhIiwib2lkIjoiMmI4ZWZkYzEtZDQwMy00MjQwLWExZDktMjhhYjM1MTI3NDgyIiwicHVpZCI6IjEwMDMzRkZGQTc5NTNBMjAiLCJzY3AiOiJ1c2VyX2ltcGVyc29uYXRpb24iLCJzdWIiOiJnREZYZEVPbk43VjdEWDlhd0RQMVdKbmtERGxUYW53c0tJZEx0NU1hNG0wIiwidGlkIjoiODc4ZjZiMzEtZmVlYS00MmJjLWE5NWQtZDQ0OTZmOWJlZjY5IiwidW5pcXVlX25hbWUiOiJsaXZlLmNvbSNwdW5lZXRndXB0YUBvdXRsb29rLmNvbSIsInV0aSI6IlNqNUJJdVB1S2ttaV9HSW9BdndsQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbIjYyZTkwMzk0LTY5ZjUtNDIzNy05MTkwLTAxMjE3NzE0NWUxMCJdfQ.o67mpIbcsnpR_SJOMGRY73NMXr4VfXXtAMOx6aA8OcV8f1VWzyB2Ovo2C6-TTySqmR01HHtT5A1phOWSEnngb3sGo26UnwAUeXCuloeZ2iuXeR0sSOcyYcAdwNn-5ELhy-oGDvYTt3z5fhNK0vhblqRl7xaob0-3epMnuPVw41i1sTT4fqh7OfqY50qr85lOZBeHF8ctEEc_iylt4wd3MKbG4JT12QTI_0Amuqiqsczbr2PtjHMp2BjSQiyRo0OjyKqvPrUKCrc0Uu0lDtgMwQvf2eVMT9k4CIvLqEE1UIXtnTg_ARzk-EsC8EZlkovi9MgkiyFJzacM5ZMKMXdNKw");
+  }*/
 
 }
